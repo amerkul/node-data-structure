1a3a00a63b6f29913b407daf30b312509ed11528
Anna Merkul <amerkul@solvd.com>
2023-09-12 14:58:43 +0300

    Implement Queue

---
+++ domain/queue.js
@@ -0,0 +1,18 @@
+export default class Queue {
+    #items = [];
+
+    constructor() {}
+
+    enqueue(item) {
+        this.#items.push(item);
+    }
+
+    dequeue() {
+        return this.#items.shift();
+    }
+
+    peek() {
+        return this.#items[0];
+    }
+
+}
