b936a997c6f6e9d1997a55cd1c24d4167b9d59e5
Anna Merkul <amerkul@solvd.com>
2023-09-12 14:51:14 +0300

    Implement Stack

---
+++ domain/stack.js
@@ -0,0 +1,18 @@
+export default class Stack {
+    #items = [];
+
+    constructor() {}
+
+    push(item) {
+        this.#items.push(item);
+    }
+
+    pop() {
+        return this.#items.pop();
+    }
+
+    peek() {
+        return this.#items[this.#items.length - 1];
+    }
+
+}
